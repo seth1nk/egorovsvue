aece7798650acb0518ceb418b894c94f5af9a178
seth1nk <kostik.vs10@mail.ru>
2025-09-26 00:20:26 +0800

    Update RoomList.vue

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -65,7 +65,7 @@ export default {
       currentPage: 1,
       totalPages: 0,
       pageSize: 10,
-      backendUrl: 'https://egorovdnode.onrender.com',
+      backendUrl: 'https://egorovsnode.onrender.com',
     };
   },
   async created() {
